16fa8f234488cda3239e19fef00656a57a422b7f
yuttpan <yuttpanin@gmail.com>
2017-09-11 07:36:00 +0700

    110901

--- src/app/model/opdhos.ts
+++ src/app/model/opdhos.ts
@@ -9,6 +9,8 @@ public tdate2 : string ,
 public date1 : Date,
 public date2 : Date  ,
 public debt_id : string ,
-public debt_code : string
+public debt_code : string,
+public paid_money : Number,
+public debt_money :Number
     ){}
 }
